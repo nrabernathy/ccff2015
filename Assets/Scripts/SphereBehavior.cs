c669a55745c2c3c9d4a7a913a3fc7d8af09992d7
Nathaniel Abernathy <nrabernathy@gmail.com>
2015-03-12 17:13:11 -0400

    Created and added project
    Added full Unity beat synchronizer projects. Unity 5.0

---
+++ Assets/Scripts/SphereBehavior.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using SynchronizerData;
+
+
+public class SphereBehavior : MonoBehaviour {
+
+	public Vector3[] beatPositions;
+
+	private BeatObserver beatObserver;
+	private int beatCounter;
+
+
+	void Start ()
+	{
+		beatObserver = GetComponent<BeatObserver>();
+		beatCounter = 0;
+	}
+
+	void Update ()
+	{
+		if ((beatObserver.beatMask & BeatType.OnBeat) == BeatType.OnBeat) {
+			//transform.position = beatPositions[beatCounter];
+			transform.Translate(1,0,0);
+			beatCounter = (++beatCounter == beatPositions.Length ? 0 : beatCounter);
+		}
+	}
+
+	void OnMouseDown()
+	{
+		transform.Translate(-1,0,0);
+	}
+}
